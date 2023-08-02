184e6826233ee4c615988533b04464dcdcab7e41
Aliaksandr Hanchar <ahanchar808@gmail.com>
2023-08-02 15:28:23 +0200

    3

--- tomcat-servlets/src/main/java/org/example/Main.java
+++ tomcat-servlets/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello new world!!!");
+        System.out.println("Hello new world!!!&");
     }
 }
